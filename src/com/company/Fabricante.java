f4d9e4c0188faddc6f456f7715d5c52b9c6cdfd3
Robertron95 <robertron561@gmail.com>
2023-01-17 13:44:26 +0100

    Actualización Fabricante

--- src/com/company/Fabricante.java
+++ src/com/company/Fabricante.java
@@ -2,5 +2,40 @@ package com.company;
 
 public class Fabricante extends Producto {
 
-    
+    private String nombre;
+    private String pais;
+
+    public Fabricante() { }
+
+    public Fabricante(String nombre, String pais) {
+
+        this.nombre = nombre;
+        this.pais = pais;
+    }
+
+    @Override
+    public String getNombre() {
+        return nombre;
+    }
+
+    @Override
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    @Override
+    public String toString() {
+        return "Fabricante{" +
+                "nombre='" + nombre + '\'' +
+                ", pais='" + pais + '\'' +
+                '}';
+    }
 }
